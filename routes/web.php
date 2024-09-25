d172bb3e0f368888069f685b4bb181ceb50dadd0
adydetra <devdewa123.sp@gmail.com>
2024-09-25 13:48:04 +0700

    fix(route): The GET method is not supported for route upload. Supported methods: POST.

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/upload', function () {
+    return view('upload');
+})->middleware(['auth', 'verified'])->name('upload');
 
 // Auth
 Route::middleware('auth')->group(function () {
